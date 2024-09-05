4f5b9c5b028b09f79c6075c8d5d3f9dba0d61a5c
PaceGG <yura3012004@gmail.com>
2024-09-05 21:06:41 +0500

    code refactor

--- globals.py
+++ globals.py
@@ -3,67 +3,46 @@ import inspect
 import os
 
 globals_file = "globals.json"
-with open(globals_file, "w") as f:
-    pass
+
+def get_caller_filename():
+    stack = inspect.stack()
+    caller_frame = stack[1]
+    return os.path.abspath(caller_frame.filename).replace("\\", "/")
+
+def read_globals():
+    if not os.path.exists(globals_file):
+        return {}
+    with open(globals_file, "r") as f:
+        return json.load(f)
+
+def write_globals(data):
+    with open(globals_file, "w") as f:
+        json.dump(data, f)
 
 def set(name, value=None):
-    stack = inspect.stack()
-    caller_frame = stack[1]
-    caller_filename = os.path.abspath(caller_frame.filename).replace("\\", "/")
-
-    with open(globals_file, "r") as f:
-        try: globals = json.load(f)[caller_filename]
-        except:
-            with open(globals_file, "w") as f:
-                json.dump({caller_filename: {}}, f)
-            with open(globals_file, "r") as f:
-                globals = json.load(f)[caller_filename]
-
+    caller_filename = get_caller_filename()
+    data = read_globals()
+    globals = data.get(caller_filename, {})
     globals[name] = value
-    
-    with open(globals_file, "r") as f:
-        data = json.load(f)
     data[caller_filename] = globals
-
-    with open(globals_file, "w") as f:
-        json.dump(data, f)
+    write_globals(data)
 
 def get(name):
-    stack = inspect.stack()
-    caller_frame = stack[1]
-    caller_filename = os.path.abspath(caller_frame.filename).replace("\\", "/")
-    
-    with open(globals_file, "r") as f:
-        globals = json.load(f)[caller_filename]
-
-    return globals[name]
+    caller_filename = get_caller_filename()
+    data = read_globals()
+    globals = data.get(caller_filename, {})
+    return globals.get(name)
 
 def remove(name):
-    stack = inspect.stack()
-    caller_frame = stack[1]
-    caller_filename = os.path.abspath(caller_frame.filename).replace("\\", "/")
-
-    with open(globals_file, "r") as f:
-        globals = json.load(f)[caller_filename]
-
+    caller_filename = get_caller_filename()
+    data = read_globals()
+    globals = data.get(caller_filename, {})
     globals.pop(name, None)
-
-    with open(globals_file, "r") as f:
-        data = json.load(f)
-
     data[caller_filename] = globals
-
-    with open(globals_file, "w") as f:
-        json.dump(data, f)
-
+    write_globals(data)
     return globals
 
 def peek():
-    stack = inspect.stack()
-    caller_frame = stack[1]
-    caller_filename = os.path.abspath(caller_frame.filename).replace("\\", "/")
-
-    with open(globals_file, "r") as f:
-        globals = json.load(f)[caller_filename]
-
-    return globals
+    caller_filename = get_caller_filename()
+    data = read_globals()
+    return data.get(caller_filename, {})
